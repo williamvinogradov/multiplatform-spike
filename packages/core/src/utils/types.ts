bdec17c9ca365e6d72af8143216706d9ffe9482f
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-11-17 17:06:57 +0400

    Remove FunctionType

--- packages/core/src/utils/types.ts
+++ packages/core/src/utils/types.ts
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type FunctionType = (...args: any[]) => any;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ObjectType = Record<PropertyKey, any>;
 export type Comparer<T> = (prev: T, next: T) => boolean;
 export type ActionFunc = () => void;
